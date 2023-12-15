099d5d2da05aa37a29dd81592c85c700579b427a
Ngai-Mint Kwan <ngai-mint.kwan@intel.com>
2023-12-15 12:01:57 +0100

    ice: Shut down VSI with "link-down-on-close" enabled
    Disabling netdev with ethtool private flag "link-down-on-close" enabled
    can cause NULL pointer dereference bug. Shut down VSI regardless of
    "link-down-on-close" state.
    
    Fixes: 8ac7132704f3 ("ice: Fix interface being down after reset with link-down-on-close flag on")
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Signed-off-by: Ngai-Mint Kwan <ngai-mint.kwan@intel.com>
    Signed-off-by: Wojciech Drewek <wojciech.drewek@intel.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -9410,6 +9410,8 @@ int ice_stop(struct net_device *netdev)
 			else
 				netdev_err(vsi->netdev, "Failed to set physical link down, VSI %d error %d\n",
 					   vsi->vsi_num, link_err);
+
+			ice_vsi_close(vsi);
 			return -EIO;
 		}
 	}
