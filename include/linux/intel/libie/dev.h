00c993ac3afa94f423668ef2b7f810e8d365cff5
Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
2026-05-15 12:07:36 +0200

    libie: define common device structure
    Signed-off-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>

---
+++ include/linux/intel/libie/dev.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2026, Intel Corporation. */
+
+#ifndef _LIBIE_DEV_H_
+#define _LIBIE_DEV_H_
+#include <linux/auxiliary_bus.h>
+
+enum libie_ieth_type {
+    LIBIE_IETH_ICE,
+    LIBIE_IETH_IXD,
+};
+
+struct libie_ieth_dev;
+
+struct libie_ieth_dev {
+    struct auxiliary_device aux;
+    enum libie_ieth_type type;
+};
+
+#endif /* _LIBIE_DEV_H_ */
